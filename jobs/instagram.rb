5691b08ca424b2fed40b6447657c5c5d6ab2f9cf
Kai Birkenfeld <mail@dyscribe.com>
2015-09-30 11:08:57 +0200

    updating instagram widget

--- jobs/instagram.rb
+++ jobs/instagram.rb
@@ -1,21 +1,21 @@
+require 'sinatra'
 require 'instagram'
-require 'rest-client'
-require 'json'
 
 # Instagram Client ID from http://instagram.com/developer
 Instagram.configure do |config|
   config.client_id = ENV['INSTAGRAM_CLIENT_ID']
   config.client_secret = ENV['INSTAGRAM_CLIENT_SECRET']
+  config.access_token = ENV['INSTAGRAM_ACCESS_TOKEN']
 end
 
-# Latitude, Longitude for location
-instadash_location_lat = '45.429522'
-instadash_location_long = '-75.689613'
+user_id = ENV['INSTAGRAM_USER_ID']
 
-SCHEDULER.every '10m', :first_in => 0 do |job|
-  photos = Instagram.media_search(instadash_location_lat,instadash_location_long)
+puts Instagram.user_recent_media("#{user_id}")
+
+SCHEDULER.every '2m', :first_in => 0 do |job|
+  photos = Instagram.user_recent_media("#{user_id}")
   if photos
-    photos.map do |photo|
+    photos.map! do |photo|
       { photo: "#{photo.images.low_resolution.url}" }
     end
   end
